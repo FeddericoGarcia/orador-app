093d86ac6738721c88606fb9d89c6c735281e3eb
FeddericoGarcia <106563428+FeddericoGarcia@users.noreply.github.com>
2023-11-23 20:09:28 -0300

    agregando interfaz y clase DAO

--- src/main/java/com/integradorcac/oradorapp/dao/iDAO.java
+++ src/main/java/com/integradorcac/oradorapp/dao/iDAO.java
@@ -1,5 +1,23 @@
-package dao;
+package com.integradorcac.oradorapp.dao;
+
+import entity.Orador;
+import java.util.List;
 
 public interface iDAO {
+    
+    public void create(Orador newOrador) throws Exception;
+    
+    public void update(Orador orador) throws Exception; 
+    
+    public Orador findById(Long id) throws Exception;
+
+    public List<Orador> search(String clave) throws Exception;
+
+    public List<Orador> findAll() throws Exception;	
+
+    public void delete(Long id) throws Exception;
+
+    public void deleteOrador(Orador orador) throws Exception;
+
     
 }
--- src/main/java/com/integradorcac/oradorapp/dao/implement/DAO.java
+++ src/main/java/com/integradorcac/oradorapp/dao/implement/DAO.java
@@ -1,5 +1,44 @@
-package dao.implement;
+package com.integradorcac.oradorapp.dao.implement;
 
-public class DAO {
+import com.integradorcac.oradorapp.dao.iDAO;
+import entity.Orador;
+import java.util.List;
+
+public class DAO implements iDAO{
+
+    @Override
+    public void create(Orador newOrador) throws Exception {
+        
+    }
+
+    @Override
+    public void update(Orador orador) throws Exception {
+        
+    }
+
+    @Override
+    public Orador findById(Long id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Orador> search(String clave) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Orador> findAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Long id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deleteOrador(Orador orador) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
