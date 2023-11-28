72daa9d685726026e436535434d9163dcb9087e2
FeddericoGarcia <106563428+FeddericoGarcia@users.noreply.github.com>
2023-11-27 21:02:49 -0300

    agregando DashboardOradorController

---
+++ src/main/java/com/integradorcac/oradorapp/controllers/DashboardOradorController.java
@@ -0,0 +1,38 @@
+package com.integradorcac.oradorapp.controllers;
+
+import com.integradorcac.oradorapp.dao.iDAO;
+import com.integradorcac.oradorapp.dao.implement.DAO;
+import entity.Orador;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardOradorController extends HttpServlet  {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        iDAO dao = DAO;
+        List<Orador> lista = new ArrayList<>();
+        
+        try{
+            lista = dao.findAll();
+        }catch(Exception e){
+            
+        }
+        req.setAttribute("lista", lista);
+        getServletContext().getRequestDispatcher("/dashboard.jsp").forward(req, resp);
+    
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); 
+    }
+    
+}
