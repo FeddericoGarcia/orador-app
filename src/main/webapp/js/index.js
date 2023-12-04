633da85c5dc003b5926bee0cc667f2b561a859ce
FeddericoGarcia <106563428+FeddericoGarcia@users.noreply.github.com>
2023-12-04 20:27:12 -0300

    agregando ultimas modificaciones de estilos y pagina de cobranza

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -27,72 +27,61 @@ btn_delete.addEventListener('click', ()=>{
 
 //Retornar resumen a traves del evento
 btn_resm.addEventListener('click', ()=>{
+    
     let cantTicket = document.getElementById("cantTicket").value;
     let categTicket = document.getElementById("categTicket").value;
     let montoTotal = (cantTicket * VALOR_TKT);
-    if(!validation(cantTicket, categTicket)){
-        categTicket == 1 ? pagarTotal.innerHTML = `${montoTotal - (DESC_EST / 100 * montoTotal)}` : 0;
-        categTicket == 2 ? pagarTotal.innerHTML = `${montoTotal - (DESC_TRA / 100 * montoTotal)}` : 0;
-        categTicket == 3 ? pagarTotal.innerHTML = `${montoTotal - (DESC_JUN / 100 * montoTotal)}` : 0;
-        categTicket == 4 ? pagarTotal.innerHTML = `${montoTotal}` : 0;
+    let total = obtenerMonto(montoTotal, cantTicket, categTicket);
     
-    };
+    pagarTotal.innerHTML = `${total}`;
 });
 
 //Validar datos inputs
 function validation(cantTicket, categTicket){
     let check = true;
     
-    if(firstName.value === ""){
-        firstName.classList.add("is-invalid");
-        console.log(firstName.value);
-        firstName.focus();
-        check = false;
-        return;
-    }
-    if(lastName.value === ""){
-        lastName.classList.add("is-invalid");
-        console.log(lastName.value);
-        lastName.focus();
-        check = false;
-        return;
-    }
+    if (isNaN(parseInt(cantTicket)) || parseInt(cantTicket) === 0){
+        if(firstName.value === ""){
+            firstName.classList.add("is-invalid");
+            console.log(firstName.value);
+            firstName.focus();
+            check = false;
+            return;
+        }
+        if(lastName.value === ""){
+            lastName.classList.add("is-invalid");
+            console.log(lastName.value);
+            lastName.focus();
+            check = false;
+            return;
+        }
 
-    if(email.value === "" || !(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value))){
-        email.classList.add("is-invalid");
-        console.log(email.value);
-        email.focus();
-        check = false;
-        return;
+        if(email.value === "" || !(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value))){
+            email.classList.add("is-invalid");
+            console.log(email.value);
+            email.focus();
+            check = false;
+            return;
+        }
+        if(parseInt(cantTicket) === 0 || isNaN(parseInt(cantTicket))){
+            cantTicket.classList.add("is-invalid");
+            console.log(cantTicket);
+            cantTicket.focus();
+            check = false;
+            return;
+        }
+        if(categTicket.value === ""){
+            categTicket.classList.add("is-invalid");
+            console.log(categTicket.value);
+            categTicket.focus();
+            check = false;
+            return;
+        }
     }
-    if(parseInt(cantTicket) === 0 || isNaN(parseInt(cantTicket))){
-        cantTicket.classList.add("is-invalid");
-        console.log(cantTicket);
-        cantTicket.focus();
-        check = false;
-        return;
-    }
-    if(categTicket.value === ""){
-        categTicket.classList.add("is-invalid");
-        console.log(categTicket.value);
-        categTicket.focus();
-        check = false;
-        return;
-    }
+}
 
-    }
-    
-// Redireccionamiento a página de cobranza
-btn_comprar.addEventListener('click', function () {
-        let monto = obtenerMonto();
-        window.location.href = "pagos360.html?monto=" + encodeURIComponent(monto);
-    });
-    
-function obtenerMonto() {
-    let cantTicket = document.getElementById("cantTicket").value;
-    let categTicket = document.getElementById("categTicket").value;
-    let montoTotal = (cantTicket * categTicket);
-    
+function obtenerMonto(montoTotal, cantTicket, categTicket) {
+
     if(!validation(cantTicket, categTicket)){
         categTicket == 1 ? montoTotal = montoTotal - (DESC_EST / 100 * montoTotal) : 0;
         categTicket == 2 ? montoTotal = montoTotal - (DESC_TRA / 100 * montoTotal) : 0;
@@ -100,8 +89,17 @@ function obtenerMonto() {
         categTicket == 4 ? montoTotal = montoTotal : 0;
     
     };
-    console.log(montoTotal);
     return montoTotal; 
 }
+
+// Redireccionamiento a página de cobranza
+btn_comprar.addEventListener('click', function () {
+    let cantTicket = document.getElementById("cantTicket").value;
+    let categTicket = document.getElementById("categTicket").value;
+    let montoTotal = (cantTicket * VALOR_TKT);
+    
+        let monto = obtenerMonto(montoTotal, cantTicket, categTicket);
+        window.location.href = "pagos360.html?monto=" + encodeURIComponent(monto);
+    });
 
 });
--- src/main/webapp/js/pagos360.js
+++ src/main/webapp/js/pagos360.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Obtén el valor del parámetro 'monto' de la URL
-    var parametros = new URLSearchParams(window.location.search);
-    var monto = parametros.get('monto');
+    let parametro = new URLSearchParams(window.location.search);
+    let monto = parametro.get('monto');
     let montoPagar = document.getElementById("montoPagar");
 
     montoPagar.innerHTML = `$ `+ monto;
